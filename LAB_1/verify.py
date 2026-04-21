ad069f951938e52682bd3101b3fa003919c578b7
Leven_hub <57728215+IMPULSE-LAB-CRYPTO@users.noreply.github.com>
2026-04-21 14:21:38 +0400

    Update verify.py

--- LAB_1/verify.py
+++ LAB_1/verify.py
@@ -1,21 +1,22 @@
 import numpy as np
-import sys
 
-def read_matrix(filename):
+def load_matrix(filename):
     with open(filename, 'r') as f:
         n = int(f.readline())
-        data = np.loadtxt(f)
-        return data.reshape((n, n))
+        return np.loadtxt(f).reshape(n, n)
 
-if __name__ == "__main__":
-    A = read_matrix("A.txt")
-    B = read_matrix("B.txt")
-    expected = A @ B # Перемножение через numpy
-    
-    actual = read_matrix("result.txt")
-    
-    if np.allclose(expected, actual, atol=1e-5):
-        print("VERIFICATION SUCCESSFUL!")
+try:
+    A = load_matrix("A.txt")
+    B = load_matrix("B.txt")
+    res_cpp = load_matrix("result.txt")
+
+    # Вычисляем эталон
+    res_py = np.dot(A, B)
+
+    # Сравниваем с погрешностью (из-за особенностей float/double)
+    if np.allclose(res_cpp, res_py, atol=1e-5):
+        print("ВЕРИФИКАЦИЯ ПРОЙДЕНА: Результаты совпадают.")
     else:
-        print("VERIFICATION FAILED!")
-        sys.exit(1)
+        print("ОШИБКА: Результаты не совпадают!")
+except Exception as e:
+    print(f"Ошибка при верификации: {e}")
